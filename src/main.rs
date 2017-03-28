e6d09cfac84e66cb8650c8c91c73bf8d052aaf27
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2017-03-27 22:37:43 -0400

    Must add a dummy test file to force a build as per
    https://github.com/RustDT/RustDT/issues/30

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+
+    opts.optflag("h", "help", "print this help menu");
+    opts.optopt("d", "database", "set configured database", "PATH");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
 }
